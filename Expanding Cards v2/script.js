5f9ddb21f007c6e80413c627a23b80a8e4676e14
JoshuaDavids <joshuadavids.jcd@gmail.com>
2025-03-10 10:12:57 +0200

    Added hoverable expanding cards

---
+++ Expanding Cards v2/script.js
@@ -0,0 +1,37 @@
+const galleryContainer = document.querySelector(".gallery");
+const galleryItems = galleryContainer.querySelectorAll(".gallery__item");
+const indicator = document.querySelector(".indicator");
+
+const defaultItemFlex = "0 1 40px";
+const hoverItemFlex = "1 1 400px";
+
+const updateGalleryItems = () => {
+  galleryItems.forEach((item) => {
+    let flex = defaultItemFlex;
+
+    if (item.isHovered) {
+      flex = hoverItemFlex;
+    }
+
+    item.style.flex = flex;
+  });
+};
+
+galleryItems[0].isHovered = true;
+updateGalleryItems();
+
+galleryItems.forEach((item) => {
+  item.addEventListener("mouseenter", () => {
+    galleryItems.forEach((otherItem) => {
+      otherItem.isHovered = otherItem === item;
+    });
+
+    updateGalleryItems();
+  });
+});
+
+galleryContainer.addEventListener("mousemove", (e) => {
+  indicator.style.left = `${
+    e.clientX - galleryContainer.getBoundingClientRect().left
+  }px`;
+});
